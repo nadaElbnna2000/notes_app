f9f564af60c730bfbcb309105dbf1e9943502721
nada <nadaelbanna105@gmail.com>
2024-09-01 21:49:22 +0200

    create notes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  //create notes
   @override
   Widget build(BuildContext context) {
     return Scaffold();
