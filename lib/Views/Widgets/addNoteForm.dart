ff1b556c5ee645d5f49ba2ed87f11afe56f0250b
nada <nadaelbanna105@gmail.com>
2024-09-03 21:25:37 +0200

    format date

--- lib/Views/Widgets/addNoteForm.dart
+++ lib/Views/Widgets/addNoteForm.dart
@@ -8,6 +8,9 @@ import 'custom_textField.dart';
 
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+
+
 
 
 class AddNoteForm extends StatefulWidget {
@@ -72,7 +75,15 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   if(formKey.currentState!.validate()){
                     formKey.currentState!.save();
 
-                    var noteModel= NoteModel(title: title!, subtitle: subtitle!, date: DateTime.now().toString(), color: Colors.blue.value);
+                    var currentDate =DateTime.now();
+                    var formattedCurrentDate = DateFormat.yMd().format(currentDate);
+
+                    var noteModel= NoteModel(
+                        title: title!,
+                        subtitle: subtitle!,
+                        date: formattedCurrentDate,
+                        color: Colors.blue.value
+                    );
 
                     BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
                   } else{
